8f8fea8a3092b7418a2c1818ed25f09d623b4045
basokant <basokanthan@gmail.com>
2024-02-17 22:44:43 -0500

    setup shadcn-svelte ui

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+<Button>Hello World</Button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
