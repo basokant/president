c60d1cf8d5cc88234903a96626bd4bf576565976
basokant <basokanthan@gmail.com>
2024-02-18 00:40:36 -0500

    add icon bounce animation

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -12,10 +12,10 @@
 <div class="z-10 flex h-full w-full flex-col items-center justify-center gap-7">
 	<div class="flex flex-col items-center justify-center gap-0.5">
 		<div class="flex w-72 justify-between">
-			<Heart strokeWidth={3} size={30} class="text-primary" />
-			<Club strokeWidth={3} size={30} />
-			<Diamond strokeWidth={3} size={30} class="text-primary" />
-			<Spade strokeWidth={3} size={30} />
+			<Heart strokeWidth={3} size={30} class="animate-bounce text-primary" />
+			<Club strokeWidth={3} size={30} class="animate-[bounce_1.2s_infinite]" />
+			<Diamond strokeWidth={3} size={30} class="animate-[bounce_1.4s_infinite] text-primary " />
+			<Spade strokeWidth={3} size={30} class="animate-[bounce_1.6s_infinite]" />
 		</div>
 		<h1 class="text-6xl font-bold">President</h1>
 		<h2 class="text-2xl font-medium">The Ultimate Social Card Game</h2>
