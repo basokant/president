96071f70d5709e09b3e00f36215408881584f15d
basokant <basokanthan@gmail.com>
2024-02-18 00:02:09 -0500

    fix buttons in dark mode

--- frontend/src/lib/components/ui/button/index.ts
+++ frontend/src/lib/components/ui/button/index.ts
@@ -6,11 +6,11 @@ const buttonVariants = tv({
 	base: 'inline-flex items-center justify-center rounded-md:w font-medium whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
 	variants: {
 		variant: {
-			default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+			default: 'bg-primary text-primary-foreground hover:bg-primary/85',
 			destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
 			outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
 			outlinePrimary:
-				'border border-primary border-2 bg-transparent hover:bg-primary-foreground text-primary',
+				'border border-primary border-2 bg-background hover:bg-primary/10 text-primary',
 			secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
 			ghost: 'hover:bg-accent hover:text-accent-foreground',
 			link: 'text-primary underline-offset-4 hover:underline'
